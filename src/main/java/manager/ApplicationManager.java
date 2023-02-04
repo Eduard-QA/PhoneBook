2aa13db8a24ecfcaad3ffc93d5daf53896bc08b6
Eduard-QA <eduard.mickel@gmail.com>
2023-02-04 19:53:22 +0200

    04.02

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,67 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+    WebDriver wd;
+    HelperUser helperUser;
+    HelperContact helperContact;
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() {
+        if (browser.equals(Browser.CHROME.browserName())){ // chrome == firefox
+            wd = new ChromeDriver();
+            logger.info("All tests start in Chrome Browser");
+        }else if(browser.equals(Browser.FIREFOX.browserName())){ // firefox === firefox
+            FirefoxOptions options = new FirefoxOptions();
+            options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+            wd = new FirefoxDriver(options);
+            logger.info("All tests star in FireFox Browser");
+        } else if (browser.equals(Browser.IE.browserName())) {  //internet explorer
+            wd = new InternetExplorerDriver();
+        } else if (browser.equals(Browser.EDGE.browserName())) { // MicrosoftEdge
+            wd = new EdgeDriver();
+        }
+
+        WebDriverListener listener=new ListenerWD();
+        wd=new EventFiringDecorator<>(listener).decorate(wd);
+
+
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wd.navigate().to("https://telranedu.web.app");
+        logger.info("Current Url -->" +wd.getCurrentUrl());
+        helperUser= new HelperUser(wd);
+        helperContact= new HelperContact(wd);
+
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
+
+    public HelperContact helperContact() {
+        return helperContact;
+    }
+}
---
+++ src/main/java/manager/HelperBase.java
@@ -0,0 +1,49 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HelperBase {
+    WebDriver wd;
+
+    public HelperBase(WebDriver wd) {
+        this.wd = wd;
+    }
+
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        element.sendKeys(Keys.COMMAND+"a");
+        element.sendKeys(Keys.BACK_SPACE);
+
+        if(text!=null){
+
+            element.sendKeys(text);
+        }
+
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+
+
+    public void test(String data){ //"30"
+
+        String locator="//div[text()='" + data+ "']";
+
+        click(By.xpath(locator));
+
+    }
+}
